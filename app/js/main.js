ac4116814f832544da79ff8ee3e2a967c4553d6a
mak-on-do <tohafedor@gmail.com>
2021-08-17 15:33:53 +0400

    Added adaptive for ipad

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,33 @@ $(function() {
         slidesToShow: 6,
         slidesToScroll: 1,
         centerMode: true,
-        autoplay: true
+        autoplay: true,
+        responsive: [{
+            breakpoint: 1367,
+            settings: {
+                slidesToShow: 5
+            }
+        }, {
+            breakpoint: 1153,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 961,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 721,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 526,
+            settings: {
+                slidesToShow: 1
+            }
+        }, ]
     });
 
     $('.trust-us-slider-2').slick({
@@ -17,7 +43,34 @@ $(function() {
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        rtl: true
+        rtl: true,
+        responsive: [{
+            breakpoint: 1281,
+            settings: {
+                slidesToShow: 5
+            }
+        }, {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 855,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 621,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 441,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true
+            }
+        }, ]
     });
 
     $('.media-about-us__slider').slick({
@@ -25,7 +78,28 @@ $(function() {
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
-        autoplay: true
+        autoplay: true,
+        responsive: [{
+            breakpoint: 1281,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 901,
+            settings: {
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 491,
+            settings: {
+                centerMode: false,
+                slidesToShow: 1
+            }
+        }, ]
+    });
+
+    $('.menu__btn').on('click', function() {
+        $('.menu').slideToggle();
     });
 
 });
